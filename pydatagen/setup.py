0b9a4278428b0fee94ec00546c3fdb25858fcec2
prrateekk <prrateekk@hotmail.com>
2017-04-29 16:21:22 +0530

    altered name

--- pydatagen/setup.py
+++ pydatagen/setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='datagen',
+setup(name='pydatagen',
       version='0.1',
       description='random data generator package',
       author='Prateek Agarwal',
       author_email='prrateekk@hotmail.com',
-      url = 'https://github.com/prrateekk/datagen'
-      packages=['datagen'],
+      url = 'https://github.com/prrateekk/pydatagen',
+      packages=['pydatagen'],
       zip_safe=False)
